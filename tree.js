12844eb688ac132cadf3bb19a3f6fd1b890a7866
Owen Lewis <owenalewis@gmail.com>
2023-08-29 16:15:09 +0100

    Imports Node factory, creates Tree factory Imports Node factory from node.js. Creates a Tree factory with an attribute "root" that uses the return value of a function called buildTree.

---
+++ tree.js
@@ -0,0 +1,13 @@
+import Node from "./node.js";
+
+// Tree factory
+const Tree = () => {
+  const root = buildTree();
+
+  return {
+    root,
+  };
+};
+
+// Function for building tree
+function buildTree(arr) {}
