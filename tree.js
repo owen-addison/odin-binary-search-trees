53f70ecb72fbcd42eafa0a9c9659e3e6bf224ed2
Owen Lewis <owenalewis@gmail.com>
2023-08-29 16:56:46 +0100

    Fixes Tree factory and initialises array points Tree now accepts an array as an argument and passes it to buildTree. The buildTree function now initialises the start, end and mid points of the array and checks the base case for when the function is called recursively.

--- tree.js
+++ tree.js
@@ -1,8 +1,8 @@
 import Node from "./node.js";
 
 // Tree factory
-const Tree = () => {
-  const root = buildTree();
+const Tree = (array) => {
+  const root = buildTree(array);
 
   return {
     root,
@@ -16,8 +16,20 @@ function buildTree(array) {
 
     1. Sort array
     2. Remove duplicates
+    3. Initialise the start, end and mid points of the array
+    4. Check base case
   */
 
   // Sort the array and remove duplicates using a sorting function and the Set() object
   const uniqSorted = [...new Set(array.sort((a, b) => a - b))];
+
+  // Initialise start, end and mid points of array
+  const start = 0;
+  const end = uniqSorted.length - 1;
+  const mid = parseInt((start + end) / 2);
+
+  // Check the base case for recursion
+  if (uniqSorted.length < 2) {
+    return null;
+  }
 }
